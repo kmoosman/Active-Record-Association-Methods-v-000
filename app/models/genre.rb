eccf6d6889312397c67d71722bd25613aa1ae34d
Katie Moosman <moosman.katie@gmail.com>
2019-02-21 03:44:53 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,9 +14,7 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    names = []
-    self.artists.tap do |artist|
-      names << artist.name
-    end
+    self.artists.first.name
+
   end
 end
